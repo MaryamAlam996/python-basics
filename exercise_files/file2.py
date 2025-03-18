57ef731759c18cafa01a08874c44734f0408f5e6
MaryamAlam996 <maryamalam996@gmail.com>
2025-03-18 13:37:15 +0000

    Adding more demo and exercise files

--- exercise_files/file2.py
+++ exercise_files/file2.py
@@ -23,4 +23,30 @@ else:
 
 # Make a new directory
 
-Path.mkdir("new_directory", )
+new_dir = current_dir / "new_directory"
+new_dir.mkdir(exist_ok=True)
+# prevent any errors if the directory already exists
+
+
+# Opening a file
+
+# Opening a file with a context manager
+with open(file_path) as file:
+    # Do stuff with the file contents
+    print(file.read())
+
+print(file)
+# context manager automatically closes the file
+# when the block of code is exited - no more reads allowed
+# print(file.read())  
+
+
+
+# Way of doing withoout context manager
+# not good practice
+opened_file = open(file_path)
+print(opened_file.read())
+opened_file.close()
+
+print(opened_file)
+
